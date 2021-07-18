c1bdc4f74e37f3e1bccefb8c361bae4e97fa2ceb
Cheyenne Atapour <cheyenneatapour@gmail.com>
2021-07-18 03:10:31 -0700

    add draw function

--- standard.py
+++ standard.py
@@ -11,6 +11,9 @@ class Card:
 	def name(self):
 		return self.val + ' of ' + self.suit
 
+	def print(self):
+		print(self.name())
+
 values = ['Ace', '2', '3', '4', '5', '6', '7', '8', '9', '10', 'Jack', 'Queen', 'King']
 suits = ['Hearts', 'Clubs', 'Diamonds', 'Spades']
 
@@ -21,17 +24,31 @@ for s in suits:
 		deck.append(Card(v, s))
 
 # Shuffle the deck
-shuffled_deck = []
-while len(shuffled_deck) < 52:
-	index = random.randint(0, len(deck) - 1)
-	shuffled_deck.append(deck[index])
-	del deck[index]
+def shuffle(deck=deck):
+	shuffled_deck = []
+	while len(shuffled_deck) < 52:
+		index = random.randint(0, len(deck) - 1)
+		shuffled_deck.append(deck[index])
+		del deck[index]
+	return shuffled_deck
 
-deck = shuffled_deck
+deck = shuffle(deck)
 
 # Returns a hand of the drawn cards
 def draw(num=1, deck=deck):
-	pass
+	hand = deck[0:num]
+	for x in range(num):
+		del deck[x]
+	return hand
+
+# Draw 2 cards
+hand = draw(2)
+for h in hand:
+	h.print()
+
+
+
+
 
 
 
